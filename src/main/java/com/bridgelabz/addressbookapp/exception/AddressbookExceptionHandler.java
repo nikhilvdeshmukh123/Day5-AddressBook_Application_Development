54940fe7866f872a42bdeda52089eeb4ff25a0ce
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-08-30 00:07:48 +0530

    Added: ExceptionHandler to show user friendly Error Response If Validation fails

---
+++ src/main/java/com/bridgelabz/addressbookapp/exception/AddressbookExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.addressbookapp.exception;
+
+import com.bridgelabz.addressbookapp.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class AddressbookExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+        List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+        List<String> errMesg = errorList.stream().map(objErr -> objErr.getDefaultMessage()).collect(Collectors.toList());
+        ResponseDto responseDto = new ResponseDto("Exception while processing REST request", errMesg);
+        return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.BAD_REQUEST);
+    }
+}
