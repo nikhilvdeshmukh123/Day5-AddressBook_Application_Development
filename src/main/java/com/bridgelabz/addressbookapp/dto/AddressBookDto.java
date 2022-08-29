0a1b557f1708c20a0e4eed5376c8858962da840c
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-08-29 19:44:27 +0530

    Modified: Use Lombok To auto generate getters and setters

--- src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDto.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDto.java
@@ -1,5 +1,10 @@
 package com.bridgelabz.addressbookapp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class AddressBookDto {
     public String firstName;
     public String lastName;
@@ -10,31 +15,4 @@ public class AddressBookDto {
     public int zipCode;
     public String emailId;
     public Long phoneNumber;
-
-    public AddressBookDto(String firstName, String lastName, String gender, String address, String city, String state, int zipCode, String emailId, Long phoneNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.gender = gender;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zipCode = zipCode;
-        this.emailId = emailId;
-        this.phoneNumber = phoneNumber;
-    }
-
-    @Override
-    public String toString() {
-        return "AddressBookDto{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", gender='" + gender + '\'' +
-                ", address='" + address + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", zipCode='" + zipCode + '\'' +
-                ", phoneNumber=" + phoneNumber +
-                ", emailId='" + emailId + '\'' +
-                '}';
-    }
 }
--- src/main/java/com/bridgelabz/addressbookapp/dto/ResponseDto.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/ResponseDto.java
@@ -1,28 +1,11 @@
 package com.bridgelabz.addressbookapp.dto;
 
-public class ResponseDto {
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
+public class ResponseDto {
     private String message;
     private Object data;
-
-    public ResponseDto(String message, Object data) {
-        this.message = message;
-        this.data = data;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
 }
